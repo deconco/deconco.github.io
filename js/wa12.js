cd8216730034c46b8df68e43d28b95498c8232c5
deconco <111922511+deconco@users.noreply.github.com>
2025-08-01 01:41:18 -0400

    wa12

--- js/wa12.js
+++ js/wa12.js
@@ -9,7 +9,7 @@ newQuote.addEventListener('click', getQuote);
 
 //Add a new variable that holds the API endpoint: 
 //https://trivia.cyberwisp.com/getrandomchristmasquestion
-const endpoint = "https://zenquotes.io/api/random";
+const endpoint = "https://geek-jokes.sameerkumar.website/api?format=json";
 
 
 let json = ''; // this makes json a global variable which means we can change it in our getQuote function and also acces it in our displayAnswer function
@@ -34,34 +34,64 @@ async function getQuote() {
         //If successful, output the quote to the console
         //console.log(json.question);  // specifies that we want ONLY the question
         //console.log(json.answer);   // or only the answer
-        displayQuote(json.question);
+        displayQuote(json.joke);
 
     } catch (err) {
-        alert('Failed to fetch a new inspirational quote.');
+        alert('Failed to fetch new Chuck Norris content.');
     }
 
 }
 
-// display the answer when the blue button is clicked
-const newAnswer = document.querySelector("#js-tweet");
-newAnswer.addEventListener('click', displayAnswer);
-
-
-function displayAnswer() {
-    const answerText = json.answer;
-    const answerArea = document.querySelector("#js-answer-text");
-    answerArea.textContent = answerText;
-}
-
-
 
 //Write a second function called "displayQuote" that will display the text of a fetched quote in the HTML element with an id of js-quote-text.
-function displayQuote(quote) {
+function displayQuote(joke) {
     const quoteText = document.querySelector('#js-quote-text');
-    quoteText.textContent = quote;
+    quoteText.textContent = joke;
 }
 
-getQuote();
 
-//Adjust getQuote to run displayQuote at the proper place in the code.
-//Notice when you refresh that a quote isn't displayed. Fix that.
+/// new stuff trying to fetch an image and then display it
+
+// event listener added to the button
+const newCat = document.querySelector("#js-tweet");
+newCat.addEventListener('click', getCat);
+
+// add the API and make myCat a global variable
+const endpoint2 = "https://cataas.com/cat?position=center&json=true";
+let myCat = "";
+
+
+async function getCat() {
+    const catArea = document.querySelector("#js-answer-text");
+    try {
+        //get a random cat from the endpoint
+        const response = await fetch(endpoint2);
+
+        //If it fails, output an error message to the console AND via alert
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+
+        myCat = await response.json();
+        // I think I want the url as the source to be able to display the picture??
+        displayCat(myCat.url);
+
+    } catch (err) {
+        alert('Failed to fetch a cat.');
+    }
+
+}
+
+// pass the url we fetched, make a new image with that url as the source,
+// and then append it to the area in the document where I want it to show up
+function displayCat(url) {
+    const catSource = json.url;
+    const catArea = document.querySelector("#cat-image");
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', catSource);
+    newImage.setAttribute('alt', "an image of a cat");
+    catArea.appendChild(newImage);
+}
+
+
+getQuote();
